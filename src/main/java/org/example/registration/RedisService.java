a12b0e1cd82e92ad290f0407748fdcd4ce0b0aed
ghenboh <1595200856@qq.com>
2024-07-21 17:39:31 +0800

    add cache

---
+++ src/main/java/org/example/registration/RedisService.java
@@ -0,0 +1,52 @@
+package org.example.registration;
+
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.RedisURI;
+import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.api.sync.RedisCommands;
+import io.lettuce.core.support.ConnectionPoolSupport;
+import org.apache.commons.pool2.impl.GenericObjectPool;
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+
+public class RedisService {
+    GenericObjectPool<StatefulRedisConnection<String, String>> redisConnectionPool;
+
+    public RedisService() {
+        RedisURI redisURI = RedisURI.Builder.redis("127.0.0.1", 6379).build();
+        RedisClient redisClient = RedisClient.create(redisURI);
+        try {
+            GenericObjectPoolConfig<StatefulRedisConnection<String, String>> poolConfig = new GenericObjectPoolConfig<>();
+            poolConfig.setMaxTotal(20);
+            poolConfig.setMaxIdle(5);
+            poolConfig.setTestOnReturn(true);
+            poolConfig.setTestWhileIdle(true);
+            redisConnectionPool = ConnectionPoolSupport.createGenericObjectPool(redisClient::connect, poolConfig);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public <T> T executeSync(SyncCommandCallback<T> command) {
+        try(StatefulRedisConnection<String, String> connection = redisConnectionPool.borrowObject()) {
+            connection.setAutoFlushCommands(true);
+            RedisCommands<String, String> syncCommands = connection.sync();
+            return command.doInConnection(syncCommands);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public String set(String k, String v) {
+        return executeSync(command -> command.set(k, v));
+    }
+
+    public String get(String k) {
+        return executeSync(command -> command.get(k));
+    }
+
+    public Long delete(String k) {
+        return executeSync(command -> command.del(k));
+    }
+
+}
--- src/main/java/org/example/registration/RegisterService.java
+++ src/main/java/org/example/registration/RegisterService.java
@@ -5,6 +5,8 @@ import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.retry.ExponentialBackoffRetry;
 import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
 
 import java.net.InetSocketAddress;
 import java.util.List;
@@ -14,6 +16,7 @@ public class RegisterService {
     private CuratorFramework client;
     private static final String ROOT = "MyRPC";
     private static Random random = new Random();
+    private static RedisService redisService = new RedisService();
 
     public RegisterService() {
         RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 3);
@@ -29,6 +32,16 @@ public class RegisterService {
             }
             String newPath = "/" + serviceName + "/" + address.getHostName() + ":" + address.getPort();
             client.create().creatingParentsIfNeeded().withMode(CreateMode.EPHEMERAL).forPath(newPath);
+            redisService.set(serviceName, address.getHostName() + ":" + address.getPort());
+            client.getData().usingWatcher(new Watcher() {
+                public void process(WatchedEvent event) {
+                    if(event.getType() == Event.EventType.NodeDataChanged) {
+                        String[] transfer = event.getPath().split("/");
+                        String serviceName = transfer[transfer.length - 1];
+                        redisService.delete(serviceName);
+                    }
+                }
+            }).forPath(newPath);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -36,14 +49,22 @@ public class RegisterService {
 
     public InetSocketAddress serviceDiscover(String serviceName) {
         try {
+            if(redisService.get(serviceName) != null) {
+                return makeAddress(redisService.get(serviceName));
+            }
             List<String> addresses = client.getChildren().forPath("/" + serviceName);
             String address = addresses.get(random.nextInt(addresses.size()));
-            String ip = address.split(":")[0];
-            int port = Integer.parseInt(address.substring(address.lastIndexOf(":") + 1));
-            return new InetSocketAddress(ip, port);
+            redisService.set(serviceName, address);
+            return makeAddress(address);
         } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
+    }
+
+    public InetSocketAddress makeAddress(String address) {
+        String ip = address.split(":")[0];
+        int port = Integer.parseInt(address.substring(address.lastIndexOf(":") + 1));
+        return new InetSocketAddress(ip, port);
     }
 }
